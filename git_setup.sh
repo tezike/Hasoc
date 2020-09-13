76489fec5e35cb1ddf9f7327c61403d98ea8ad34
tezike <eziket18@gmail.com>
2020-09-13 15:46:20 +0000

    modified to allow branch name show in terminal

--- git_setup.sh
+++ git_setup.sh
@@ -7,3 +7,17 @@ git init
 git remote rm origin
 git remote add origin https://$USERNAME:$PASSWORD@github.com/$USERNAME/${PWD##*/}.git
 git pull origin master --allow-unrelated-histories
+
+# Make jupyter terminal show branch name
+# git_branch() {
+#   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+# }
+# export PS1="[\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$(git_branch)\$ "
+
+echo "git_branch() {" >> ~/.bashrc
+echo "      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'" >> ~/.bashrc
+echo "    }" >> ~/.bashrc
+echo 'export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$ (git_branch)\$ "' >> ~/.bashrc
+
+# in the terminal, type
+# source ~/.bashrc
