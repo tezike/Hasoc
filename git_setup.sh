a3c4812809ce4ddcef9838dfb27c711259aff997
tezike <eziket18@gmail.com>
2020-09-13 15:55:51 +0000

    fixed bug to allow branch show properly

--- git_setup.sh
+++ git_setup.sh
@@ -17,7 +17,7 @@ git pull origin master --allow-unrelated-histories
 echo "git_branch() {" >> ~/.bashrc
 echo "      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'" >> ~/.bashrc
 echo "    }" >> ~/.bashrc
-echo 'export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$ (git_branch)\$ "' >> ~/.bashrc
+echo 'export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$(git_branch)\$ "' >> ~/.bashrc
 
 # in the terminal, type
 # source ~/.bashrc
