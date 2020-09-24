ae52e4cc6387f5069818b87a4c2b2970a9faffcf
Ezike Tochukwu <31063326+tezike@users.noreply.github.com>
2020-09-24 08:44:18 +0100

    Update

--- fix_nb_checkout.sh
+++ fix_nb_checkout.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #this fixes the breaking changes that occur to .ipynb files once  a checkout happens
 # stolen from https://github.com/fastai/course-v3/issues/64
 perl -pi -e 's|execution_count": null|execution_count": 1|g' nbs/*ipynb
